0da8eb981549d3ac9816b83a88c4dcbb500f7dfb
stefkonoisyboy <72032474+stefkonoisyboy@users.noreply.github.com>
2021-01-15 11:34:51 +0200

    Entity added

---
+++ StudentsSocialMedia.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentsSocialMedia.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "StudentsSocialMedia";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
